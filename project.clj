eba62899a401b6fbc132588c558778e4df4547e0
AkBacancy <80810815+AkBacancy@users.noreply.github.com>
2023-08-19 16:41:46 +0530

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject my-clojure-project "1.0.0"
+  :dependencies [[cheshire "1.10.3"]
+                 [compojure "0.1.0"]
+                 [clj-http "0.1.1"]
+                 [hiccup "1.1.2"]
+                 [clj-time "1.2.0"]
+                 [reagent "1.3.0"]
+                 [ring/ring-defaults "1.4.0"]
+                 [ring/ring-jetty-adapter "1.2.0"]
+                 [com.novemberain/monger "1.4.0"]])
